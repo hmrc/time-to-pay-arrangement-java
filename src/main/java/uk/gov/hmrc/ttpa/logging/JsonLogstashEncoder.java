cf61e11a4ecec6bb5fe271f532606deb7532e014
aminmc <aminmc@gmail.com>
2016-09-15 09:32:06 +0100

    SSTTP-275 Initial commit for logging

---
+++ src/main/java/uk/gov/hmrc/ttpa/logging/JsonLogstashEncoder.java
@@ -0,0 +1,67 @@
+package uk.gov.hmrc.ttpa.logging;
+
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.ThrowableProxyUtil;
+import ch.qos.logback.core.encoder.EncoderBase;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.apache.commons.io.IOUtils.LINE_SEPARATOR;
+import static org.apache.commons.io.IOUtils.write;
+
+public class JsonLogstashEncoder extends EncoderBase<ILoggingEvent> {
+
+    private final ObjectMapper mapper = new ObjectMapper().configure(JsonGenerator.Feature.ESCAPE_NON_ASCII, true);
+
+    private static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss.SSSZZ";
+
+    @Override
+    public void doEncode(ILoggingEvent event) throws IOException {
+        final ObjectNode eventNode = mapper.createObjectNode();
+
+        String app = resolveAppName();
+        eventNode.put("app", app);
+        eventNode.put("hostname", InetAddress.getLocalHost().getHostName());
+        eventNode.put("timestamp", convertTime(event.getTimeStamp()));
+        eventNode.put("message", event.getFormattedMessage());
+
+        if (event.getThrowableProxy() != null) {
+            eventNode.put("exception", ThrowableProxyUtil.asString(event.getThrowableProxy()));
+        }
+        eventNode.put("logger", event.getLoggerName());
+        eventNode.put("thread", event.getThreadName());
+        eventNode.put("level", event.getLevel().toString());
+
+        if (getContext() != null) {
+            getContext().getCopyOfPropertyMap()
+                    .forEach((k, v) -> eventNode.put(k.toLowerCase(), v));
+        }
+
+        event.getMDCPropertyMap().forEach((k, v) -> eventNode.put(k.toLowerCase(), v));
+
+        write(mapper.writeValueAsBytes(eventNode), outputStream);
+        write(LINE_SEPARATOR, outputStream);
+        outputStream.flush();
+    }
+
+    private String resolveAppName() {
+        return System.getProperty("spring.application.name");
+    }
+
+    @Override
+    public void close() throws IOException {
+        write(LINE_SEPARATOR, outputStream);
+    }
+
+
+    public String convertTime(long time) {
+        return new SimpleDateFormat(DATE_TIME_FORMAT).format(new Date(time));
+    }
+}
