aae5701a20eac371e4ae3ed2b89cab642954c200
aminmc <aminmc@gmail.com>
2016-09-19 14:38:29 +0100

    SSTTP-267 Logging updates

--- src/main/java/uk/gov/hmrc/ttpa/TermsAndConditionsController.java
+++ src/main/java/uk/gov/hmrc/ttpa/TermsAndConditionsController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 @Slf4j
 @RequestMapping("/terms")
@@ -13,7 +15,7 @@ public class TermsAndConditionsController {
 
     @GetMapping
     public String terms() {
-        log.info("Terms requested..");
+        log.info("Terms requested.. '{}'", LocalDateTime.now());
         return "Welcome to TTPA";
     }
 }
