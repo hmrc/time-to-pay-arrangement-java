5af99586aab30edbffc270fec2206630d327f2d0
aminmc <aminmc@gmail.com>
2016-09-20 18:25:59 +0100

    SSTTP-274 Initial commit for metrics

--- src/main/java/uk/gov/hmrc/ttpa/metrics/MetricsProperties.java
+++ src/main/java/uk/gov/hmrc/ttpa/metrics/MetricsProperties.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @ConfigurationProperties(
-        prefix = "microservice.metrics"
+        prefix = "metrics"
 )
 @Data
 @Component
