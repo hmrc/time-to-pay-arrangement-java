af1f621c2813d7f3864d6d95e8a5759df067029f
aminmc <aminmc@gmail.com>
2016-09-20 18:30:11 +0100

    SSTTP-274 Moved post construct to config instead of the controller

--- src/main/java/uk/gov/hmrc/ttpa/ApplicationConfig.java
+++ src/main/java/uk/gov/hmrc/ttpa/ApplicationConfig.java
@@ -1,19 +1,45 @@
 package uk.gov.hmrc.ttpa;
 
+import ch.qos.logback.classic.Logger;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.json.MetricsModule;
+import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
+import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
+import com.codahale.metrics.jvm.ThreadStatesGaugeSet;
+import com.codahale.metrics.logback.InstrumentedAppender;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmrc.ttpa.metrics.InstrumentedFilter;
+import uk.gov.hmrc.ttpa.metrics.MetricsProperties;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Boolean.FALSE;
+import static java.util.Optional.ofNullable;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 @Configuration
 @EnableConfigurationProperties
+@Slf4j
 public class ApplicationConfig {
+
+    @Autowired
+    private MetricsProperties metricsProperties;
+
+    @Autowired
+    private MetricRegistry metricRegistry;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
 
     @Bean
     public FilterRegistrationBean instrumentedFilter() {
@@ -44,5 +70,34 @@ public class ApplicationConfig {
         public void contextDestroyed(ServletContextEvent sce) {
 
         }
+    }
+
+
+    @PostConstruct
+    public void init() {
+        metricRegistry.registerAll(new GarbageCollectorMetricSet());
+        metricRegistry.registerAll(new MemoryUsageGaugeSet());
+        metricRegistry.registerAll(new ThreadStatesGaugeSet());
+
+        Logger logger = (Logger) log;
+
+        InstrumentedAppender instrumentedAppender = new InstrumentedAppender(metricRegistry);
+        instrumentedAppender.setContext(logger.getLoggerContext());
+        instrumentedAppender.start();
+
+
+        TimeUnit rateUnit =
+                ofNullable(metricsProperties.getRateUnit())
+                        .orElse(SECONDS);
+
+        TimeUnit durationUnit = ofNullable(metricsProperties.getDurationUnit())
+                .orElse(SECONDS);
+
+        Boolean showSamples = ofNullable(metricsProperties.getShowSamples())
+                .orElse(FALSE);
+
+        MetricsModule metricsModule = new MetricsModule(rateUnit, durationUnit, showSamples);
+        objectMapper.registerModule(metricsModule);
+
     }
 }
